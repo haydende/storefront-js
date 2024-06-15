f1557679702936884080cebfa2ac84a505069987
Hayden Eastwell <haydeneastwell@outlook.com>
2024-06-15 14:55:33 +0100

    [server] Moved used/uncommented jest config options to top

--- storefront-server/jest.config.mjs
+++ storefront-server/jest.config.mjs
@@ -5,6 +5,19 @@
 
 /** @type {import('jest').Config} */
 const config = {
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A map from regular expressions to paths to transformers
+  transform: {},
+
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -17,22 +30,16 @@ const config = {
   // Automatically clear mock calls, instances, contexts and results before every test
   // clearMocks: false,
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
   //   "\\\\node_modules\\\\"
   // ],
 
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -173,8 +180,6 @@ const config = {
   // This option allows use of a custom test runner
   // testRunner: "jest-circus/runner",
 
-  // A map from regular expressions to paths to transformers
-  transform: {},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
